fe5d992d336322533e4618222658bb3192ad642d
Buddha <buddhagautam231@gmail.com>
2023-08-18 22:25:49 +0545

    feat: non logged in user can explore blogs

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import  IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from .serializers import BlogSerializer
 from .models import Blog
@@ -12,111 +12,9 @@ from rest_framework.views import APIView
 from rest_framework.exceptions import PermissionDenied
 
 
-# class BlogView(APIView):
-#     permission_classes = [AllowAny]
-#
-#     def post(self, request):
-#         if not request.user.is_authenticated:
-#             return Response(
-#                 {
-#                     "error": True,
-#                     "error_msg": "You must be logged in to create a blog",
-#                 },
-#                 status=status.HTTP_401_UNAUTHORIZED,
-#             )
-#         serializer = BlogSerializer(data=request.data)
-#         if serializer.is_valid(raise_exception=ValueError):
-#             serializer.save(author=self.request.user)
-#             return Response(
-#                 serializer.data,
-#                 status=status.HTTP_201_CREATED,
-#             )
-#         return Response(
-#             {
-#                 "error": True,
-#                 "error_msg": serializer.error_messages,
-#             },
-#             status=status.HTTP_400_BAD_REQUEST,
-#         )
-#
-#     def get(self, request, pk=None):
-#         if pk:
-#             blog = Blog.objects.get(id=pk)
-#             serializer = BlogSerializer(blog)
-#             return Response(
-#                 serializer.data,
-#                 status=status.HTTP_200_OK,
-#             )
-#         blogs = Blog.objects.all()
-#         serializer = BlogSerializer(blogs, many=True)
-#         return Response(
-#             serializer.data,
-#             status=status.HTTP_200_OK,
-#         )
-#
-#     def put(self, request, pk):
-#         if not request.user.is_authenticated:
-#             return Response(
-#                 {
-#                     "error": True,
-#                     "error_msg": "You must be logged in to update a blog",
-#                 },
-#                 status=status.HTTP_401_UNAUTHORIZED,
-#             )
-#         blog = Blog.objects.get(id=pk)
-#         if blog.author != request.user:
-#             return Response(
-#                 {
-#                     "error": True,
-#                     "error_msg": "You can only update your own blog",
-#                 },
-#                 status=status.HTTP_401_UNAUTHORIZED,
-#             )
-#         serializer = BlogSerializer(blog, data=request.data)
-#         if serializer.is_valid(raise_exception=ValueError):
-#             serializer.save()
-#             return Response(
-#                 serializer.data,
-#                 status=status.HTTP_200_OK,
-#             )
-#         return Response(
-#             {
-#                 "error": True,
-#                 "error_msg": serializer.error_messages,
-#             },
-#             status=status.HTTP_400_BAD_REQUEST,
-#         )
-#
-#     def delete(self, request, pk):
-#         if not request.user.is_authenticated:
-#             return Response(
-#                 {
-#                     "error": True,
-#                     "error_msg": "You must be logged in to delete a blog",
-#                 },
-#                 status=status.HTTP_401_UNAUTHORIZED,
-#             )
-#         blog = Blog.objects.get(id=pk)
-#         if blog.author != request.user:
-#             return Response(
-#                 {
-#                     "error": True,
-#                     "error_msg": "You can only delete your own blog",
-#                 },
-#                 status=status.HTTP_401_UNAUTHORIZED,
-#             )
-#         blog.delete()
-#         return Response(
-#             {
-#                 "error": False,
-#                 "error_msg": "Blog deleted successfully",
-#             },
-#             status=status.HTTP_200_OK,
-#         )
-#
 class BlogView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = BlogSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Blog.objects.all()
 
     def perform_destroy(self, instance):
@@ -142,7 +40,7 @@ class BlogView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class BlogCreateView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = BlogSerializer
     queryset = Blog.objects.all()
 
@@ -157,6 +55,7 @@ class BlogSelfView(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         return user.blogs.all()
+
 
 class BlogLikeView(APIView):
     def get(self, request, pk):
@@ -183,4 +82,3 @@ class BlogLikeView(APIView):
                 },
                 status=status.HTTP_200_OK,
             )
-
